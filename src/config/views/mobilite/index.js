f89442f46ba2071b3835402c1e30c756b4977919
Aline <chetta.aline@gmail.com>
2019-05-23 11:32:10 +0200

    Add theme mobility

---
+++ src/config/views/mobilite/index.js
@@ -0,0 +1,21 @@
+import layersTree from './layersTree';
+import interactions from './interactions';
+import customStyle from './customStyle';
+
+export default {
+  title: 'Mobilité',
+  layersTree,
+  interactions,
+  map: {
+    accessToken: 'pk.eyJ1IjoibWFraW5hY29ycHVzIiwiYSI6ImNqY3E4ZTNwcTFta3ozMm80d2xzY29wM2MifQ.Nwl_FHrWAIQ46s_lY0KNiQ',
+    backgroundStyle: 'mapbox://styles/makinacorpus/cjpdvdqdj0b7a2slajmjqy3py',
+    center: [-1.686401, 48.106514],
+    zoom: 7,
+    maxZoom: 19.9,
+    minZoom: 7,
+    customStyle,
+    fitBounds: {
+      coordinates: [[-6.0002640, 46.8491442], [1.9477370, 49.5994025]],
+    },
+  },
+};
---
+++ src/config/views/mobilite/layersTree.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    group: 'Navette Domicile - Travail',
+    layers: [
+      {
+        label: 'Transport en commun',
+        layers: ['terralego-transports'],
+        legends: [
+          {
+            title: 'Part des actifs se déplaçant en transport en commun (en %)',
+            items: [
+              { label: 'Inférieur à 1.2%', color: '#EFE3CF' },
+              { label: 'De 1.2 à 2.7%', color: '#F7C99E' },
+              { label: 'De 2.7 à 4.5%', color: '#F9AF79' },
+              { label: 'De 4.5 à 7.0%', color: '#F79465' },
+              { label: 'De 7.0 à 9.2%', color: '#E8705D' },
+              { label: 'De 9.2 à 14.1%', color: '#D4495A' },
+              { label: 'Supérieur ou égal à 14.1%', color: '#BC205D' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
