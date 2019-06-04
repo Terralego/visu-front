f47d9695583d225b955f53381929f9082b707b4d
marianne lajante <marianne.lajante@makina-corpus.com>
2019-06-04 10:29:27 +0200

    change bounds coordinates

--- src/config/views/mobilite/index.js
+++ src/config/views/mobilite/index.js
@@ -9,13 +9,13 @@ export default {
   map: {
     accessToken: 'pk.eyJ1IjoibWFraW5hY29ycHVzIiwiYSI6ImNqdzF6eWhhbDBwYnc0N3BscHNoM3Y2NWQifQ.miNBzyxOCefDL8t-CodGhA',
     backgroundStyle: 'mapbox://styles/makinacorpus/cjpdvdqdj0b7a2slajmjqy3py',
-    center: [-1.686401, 48.106514],
-    zoom: 7,
+    center: [-2.028, 47.852],
+    zoom: 7.7,
     maxZoom: 19.9,
     minZoom: 7,
     customStyle,
     fitBounds: {
-      coordinates: [[-6.0002640, 46.8491442], [1.9477370, 49.5994025]],
+      coordinates: [[-4.850, 46.776], [-0.551, 48.886]],
     },
   },
 };
