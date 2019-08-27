575226dc71ccff654f5b1508b15f93ba5ec096f2
marianne lajante <marianne.lajante@makina-corpus.com>
2019-08-27 09:55:05 +0200

    Adapt map control buttons tooltip text to Auran

--- src/views/Visualizer/View/translate.js
+++ src/views/Visualizer/View/translate.js
@@ -11,6 +11,12 @@ export default function translate (key, params) {
         return `(${params.count} résultats trouvés)`;
       }
       return '(1 résultat trouvé)';
+    case 'terralego.map.print_control.button_label':
+      return 'Print to PDF';
+    case 'terralego.map.permalink_control.button_label':
+      return 'Share your map';
+    case 'terralego.map.home_control.button_label':
+      return 'Reset map center';
     default:
       return key;
   }
