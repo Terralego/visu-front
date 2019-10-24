6018daa7ed74f8b30be4a8a0667fb55c182864bb
Aline <chetta.aline@gmail.com>
2019-10-24 11:33:07 +0200

    Fix test

--- src/services/visualizer.js
+++ src/services/visualizer.js
@@ -1,4 +1,4 @@
-import { Api } from '@terralego/core';
+import Api from '@terralego/core/modules/Api';
 import { sortCustomLayers } from '@terralego/core/modules/Visualizer/services/layersTreeUtils';
 
 import population from '../images/population.png';
