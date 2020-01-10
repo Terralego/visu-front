a9d554e49aa20c5ae2c121ab4c470731dc005983
Aline <chetta.aline@gmail.com>
2020-01-10 15:48:51 +0100

    Add proptypes + test in HeaderButton

---
+++ src/views/Main/Header/headerButton.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { HeaderButton } from './HeaderButton';
+
+it('should render correctly', () => {
+  const tree = renderer.create((
+    <HeaderButton id="theme" />
+  )).toJSON();
+  expect(tree).toMatchSnapshot();
+});
