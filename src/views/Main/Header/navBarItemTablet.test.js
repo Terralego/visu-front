6add4dd41ad421517cbbae7290ce4c84f2e22009
Aline <chetta.aline@gmail.com>
2020-01-20 10:10:24 +0100

    Remove double parenthesis

--- src/views/Main/Header/navBarItemTablet.test.js
+++ src/views/Main/Header/navBarItemTablet.test.js
@@ -6,8 +6,8 @@ jest.mock('./HeaderLink', () => () => 'HeaderLink');
 jest.mock('./HeaderButton', () => () => 'HeaderButton');
 
 it('should render correctly the component', () => {
-  const tree = renderer.create((
-    <NavBarItemTablet env={{ VIEW_ROOT_PATH: 'visualiser' }} id="test" />
-  )).toJSON();
+  const tree = renderer.create(
+    <NavBarItemTablet env={{ VIEW_ROOT_PATH: 'visualiser' }} id="test" />,
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/views/Main/Header/partnerPage.test.js
+++ src/views/Main/Header/partnerPage.test.js
@@ -3,8 +3,8 @@ import renderer from 'react-test-renderer';
 import PartnerPage from './PartnerPage';
 
 it('should render correctly', () => {
-  const tree = renderer.create((
-    <PartnerPage />
-  )).toJSON();
+  const tree = renderer.create(
+    <PartnerPage />,
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
