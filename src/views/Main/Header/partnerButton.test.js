a89ca7fdf18ba10f17c8a43e6e5e6add301cd313
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-10 11:33:24 +0200

    Refacto PartnerButton component

--- src/views/Main/Header/partnerButton.test.js
+++ src/views/Main/Header/partnerButton.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import { PartnerButton } from './PartnerButton';
 
-jest.mock('./NavBarItemTablet', () => () => <div className="container">NavBarItemTablet</div>);
-jest.mock('./NavBarItemDesktop', () => () => <div className="container">NavBarItemDesktop</div>);
+jest.mock('@terralego/core/components/NavBarItemTablet', () => () => <div className="container">NavBarItemTablet</div>);
+jest.mock('@terralego/core/components/NavBarItemDesktop', () => () => <div className="container">NavBarItemDesktop</div>);
 
 it('should render correctly in mode desktop', () => {
   const tree = renderer.create(
@@ -18,16 +17,4 @@ it('should render correctly in mode mobile', () => {
     <PartnerButton isMobileSized />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-it('should update isOpen state when open/close is called', () => {
-  const wrapper = shallow(
-    <PartnerButton />,
-  );
-
-  expect(wrapper.state('isOpen')).toBe(false);
-  wrapper.instance().open();
-  expect(wrapper.state('isOpen')).toBe(true);
-  wrapper.instance().close();
-  expect(wrapper.state('isOpen')).toBe(false);
 });
