96dc44255e678027ab4c6c5abd0eeeabd9a71974
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-10 11:33:24 +0200

    Refacto PartnerButton component

--- src/views/Main/Header/PartnerButton.js
+++ src/views/Main/Header/PartnerButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
@@ -9,55 +9,49 @@ import {
 
 import withDeviceSize from '@terralego/core/hoc/withDeviceSize';
 
-import NavBarItemDesktop from './NavBarItemDesktop';
-import NavBarItemTablet from './NavBarItemTablet';
+import NavBarItemDesktop from '@terralego/core/components/NavBarItemDesktop';
+import NavBarItemTablet from '@terralego/core/components/NavBarItemTablet';
+
 import PartnerPage from './PartnerPage';
 
-export class PartnerButton extends React.Component {
-  state = {
-    isOpen: false,
-  };
+export const PartnerButton = ({ isMobileSized, isPhoneSized, ...props }) => {
+  const [isOpen, setOpen] = useState(false);
+  const NavBarItem = isMobileSized ? NavBarItemTablet : NavBarItemDesktop;
 
-  static propTypes = {
-    isMobileSized: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    isMobileSized: false,
-  };
-
-  open = () => this.setState({ isOpen: true });
-
-  close = () => this.setState({ isOpen: false });
-
-  render () {
-    const { isOpen } = this.state;
-    const { isMobileSized } = this.props;
-    const { open, close } = this;
-    const NavBarItem = isMobileSized ? NavBarItemTablet : NavBarItemDesktop;
-    return (
-      <>
-        <NavBarItem {...this.props} onClick={open} />
-        <Overlay
+  return (
+    <>
+      <NavBarItem {...props} onClick={() => setOpen(true)} />
+      <Overlay
+        className={classNames(
+          Classes.OVERLAY_SCROLL_CONTAINER,
+          Classes.LIGHT,
+          'modal-mentions-legales',
+        )}
+        isOpen={isOpen}
+        onClose={() => setOpen(false)}
+      >
+        <div
           className={classNames(
-            Classes.OVERLAY_SCROLL_CONTAINER,
-            Classes.LIGHT,
-            'modal-mentions-legales',
+            Classes.CARD,
+            Classes.ELEVATION_4,
           )}
-          isOpen={isOpen}
-          onClose={close}
         >
-          <div
-            className={classNames(
-              Classes.CARD,
-              Classes.ELEVATION_4,
-            )}
-          >
-            <PartnerPage />
-          </div>
-        </Overlay>
-      </>
-    );
-  }
-}
+          <PartnerPage />
+        </div>
+      </Overlay>
+    </>
+  );
+};
+
+PartnerButton.propTypes = {
+  isMobileSized: PropTypes.bool,
+  isPhoneSized: PropTypes.bool,
+};
+
+PartnerButton.defaultProps = {
+  isMobileSized: false,
+  isPhoneSized: false,
+};
+
+
 export default withDeviceSize()(PartnerButton);
