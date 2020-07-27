e8a12a761d01fd92489866e3177b35206bb59047
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-07-27 14:22:42 +0200

    Try to remove SplashScreen if it exists

--- src/views/Main/Content/SplashScreen.js
+++ src/views/Main/Content/SplashScreen.js
@@ -4,7 +4,8 @@ export const SplashScreen = ({ children }) => {
   useEffect(() => {
     document.body.classList.remove('with-splash');
     setTimeout(() => {
-      document.body.querySelector('.splash-screen_container').remove();
+      const splashScreen = document.querySelector('.splash-screen_container');
+      splashScreen && splashScreen.remove();
     }, 5000);
   }, []);
   return children;
