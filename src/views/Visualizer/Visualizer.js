8ae0010aa5aee4f423af6e05ba15b7bd0da64bef
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-08-24 17:14:26 +0200

    Reset current state on authentication

--- src/views/Visualizer/Visualizer.js
+++ src/views/Visualizer/Visualizer.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import search from '@terralego/core/modules/Visualizer/services/search';
 import { connectAuthProvider } from '@terralego/core/modules/Auth';
 
+import { connectState } from '@terralego/core/modules/State/context';
+
 import { fetchViewConfig } from '../../services/visualizer';
 import Loading from './Loading';
 import View from './View';
@@ -13,11 +15,13 @@ export const Visualizer = ({
   match: { params: { viewName } },
   env: { API_HOST },
   authenticated,
+  setCurrentState,
   ...props
 }) => {
   const [loading, setLoading] = useState(true);
   const [notfound, setNotFound] = useState(false);
   const [viewConfig, setViewConfig] = useState(null);
+  const [prevAuth, setPrevAuth] = useState(null);
 
   const isUnmount = React.useRef(false);
   isUnmount.current = false;
@@ -67,7 +71,15 @@ export const Visualizer = ({
 
   useEffect(() => {
     fetchViewConfig.clear();
-  }, [authenticated]);
+    // When we login or logout, we want to reset current state
+    // but not on initial loading as we want the state from the hash to be conserved
+    // So if prevAuth is null, then we are loading the app for the first time.
+    // Otherwise, only on auth change, we reset the state.
+    if (prevAuth !== null && authenticated !== prevAuth) {
+      setCurrentState({ layers: undefined });
+    }
+    setPrevAuth(authenticated);
+  }, [authenticated, prevAuth, setCurrentState]);
 
   useEffect(() => {
     loadViewConfig(viewName);
@@ -90,4 +102,4 @@ export const Visualizer = ({
   );
 };
 
-export default connectAuthProvider('authenticated')(Visualizer);
+export default connectState('setCurrentState')(connectAuthProvider('authenticated')(Visualizer));
