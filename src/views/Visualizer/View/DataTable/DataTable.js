f8e32a2a0b746f5d0ab79074eef15443031246f9
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-09-23 11:14:10 +0200

    Fix es7 compat

--- src/views/Visualizer/View/DataTable/DataTable.js
+++ src/views/Visualizer/View/DataTable/DataTable.js
@@ -222,8 +222,9 @@ export class DataTable extends React.Component {
       }, []),
     });
 
-    const { hits: { hits, total: resultsTotal } } = resp;
+    const { hits: { hits, total: { value: resultsTotal } } } = resp;
     const newColumns = columns || extractColumns(fields, hits);
+
     const results = sort(prepareData(newColumns, hits, {
       decimals: 1,
     }));
--- src/views/Visualizer/View/View.js
+++ src/views/Visualizer/View/View.js
@@ -417,13 +417,14 @@ export class Visualizer extends React.Component {
         .map(({ id }) => id),
     }));
 
-    const totalFeatures = idsResponses.reduce((fullTotal, { hits: { total = 0 } = {} }) =>
+    const totalFeatures = idsResponses.reduce((fullTotal,
+      { hits: { total: { value: total = 0 } = {} } = {} }) =>
       fullTotal + total,
     0);
 
     this.setLayersResult(filters.map(({ layer }, index) => {
       const total = countResponses[index].hits
-        ? countResponses[index].hits.total
+        ? countResponses[index].hits.total.value
         : null;
       return { layer, state: { total } };
     }));
@@ -502,7 +503,7 @@ export class Visualizer extends React.Component {
       label, layers: resultsLayers, filters: { mainField },
     }], index) => ({
       group: label,
-      total: responses[index].hits.total,
+      total: responses[index].hits.total.value,
       results: responses[index].hits.hits.map(({ _id: id, _source: source }) => ({
         label: source[mainField] || id,
         id,
