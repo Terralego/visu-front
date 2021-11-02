12b91d0bc9187fed702a32b9d512ce67fc5c621b
bruhnild <marinefaucher@icloud.com>
2021-11-02 16:08:44 +0100

    Set enableDetailCarrousel to true by default

--- src/views/Visualizer/View/View.js
+++ src/views/Visualizer/View/View.js
@@ -131,7 +131,7 @@ export class Visualizer extends React.Component {
     initLayersState () { },
     initialState: {},
     isMobileSized: false,
-    enableDetailCarrousel: false,
+    enableDetailCarrousel: true,
   };
 
   state = {
