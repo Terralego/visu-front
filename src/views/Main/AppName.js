985a7248fe956a6f167d773b1ea95878a1cd7814
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2023-07-27 16:34:36 +0200

    Allow AppName component element & props customization

--- src/views/Main/AppName.js
+++ src/views/Main/AppName.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export const AppName = ({ title = 'TerraVisu', version = 'v1.0' }) => (
-  <div className="bp3-dark appName">
+export const AppName = ({
+  component: Component = 'div',
+  className,
+  title = 'TerraVisu',
+  version = 'v1.0',
+  ...rest
+}) => (
+  <Component
+    className={classnames(
+      'bp3-dark',
+      'appName',
+      className,
+    )}
+    {...rest}
+  >
     <span className="appName-title">
       { title }
     </span>
     <span className="appName-version">{ version }</span>
-  </div>
+  </Component>
 );
 
 
