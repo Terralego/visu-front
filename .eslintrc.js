6172fa4cbf7a5eec94734ae488fd50f92362a58b
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2023-11-14 17:29:15 +0100

    Merge linting of terra-front & visu-front

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,37 @@
+const error  = 'error';
+const warn   = 'warn';
+const off    = 'off';
+const always = 'always';
+
 module.exports = {
   extends: 'makina',
+
+  settings: {
+    'import/resolver': { // Load webpack.config to allow resolving aliases in imports
+      webpack: {
+        config: 'webpack.config.eslint.js',
+      }
+    },
+  },
+
   rules: {
-    'jsx-a11y/label-has-associated-control': ['error', {
+    'import/no-cycle': [off],
+    'import/no-unresolved': [warn, { ignore: ['^@storybook'] }],
+    'jsx-a11y/label-has-associated-control': [error, {
       controlComponents: ['InputGroup'],
     }],
+    'operator-linebreak': [off],
+    'react/destructuring-assignment': [error],
+    'react/no-access-state-in-setstate': [error],
+    'react/no-this-in-sfc': [error],
   },
+
+  overrides: [
+    {
+      files: ['src/terra-front/**/*.js'],
+      rules: {
+        camelcase: [off],
+      },
+    }
+  ],
 };
