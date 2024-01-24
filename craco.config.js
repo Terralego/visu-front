44e1d9afd00b32873e7b0e73b42ec62c0cd18c77
Maxime Bouveron <maxime.bouveron@gmail.com>
2024-01-24 15:37:34 +0100

    Upgrade blueprint to 4

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,36 @@
 const path = require('path');
+// const inliner = require('@vgrid/sass-inline-svg');
+const { sassNull } = require('sass');
 
 module.exports = {
+  style: {
+    sass: {
+      loaderOptions: {
+        sassOptions: {
+          includePaths: ['src', './node_modules'],
+          functions: {
+            /**
+             * Sass function to inline a UI icon svg and change its path color.
+             *
+             * Usage:
+             * svg-icon("16px/icon-name.svg", (path: (fill: $color)) )
+             */
+
+            'svg-icon($path, $selectors: null)': () => sassNull,
+            // inliner(
+            //   path.join(__dirname, 'ressources/icons'),
+            //   {
+            //     // run through SVGO first
+            //     optimize: true,
+            //     // minimal "uri" encoding is smaller than base64
+            //     encodingFormat: 'uri',
+            //   },
+            // ),
+          },
+        },
+      },
+    },
+  },
   webpack: {
     alias: {
       '@terralego/core': path.resolve(__dirname, 'src/terra-front'),
