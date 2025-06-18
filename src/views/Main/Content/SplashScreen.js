ada937eb6b617e33c0a484b14ed3b161e944de72
Maxime Bouveron <maxime.bouveron@gmail.com>
2025-06-18 15:17:41 +0200

    Get splashscreen from backend settings

--- src/views/Main/Content/SplashScreen.js
+++ src/views/Main/Content/SplashScreen.js
@@ -1,14 +1,24 @@
 import { useEffect } from 'react';
+import { connectSettings } from '../Provider/context';
 
-export const SplashScreen = ({ children }) => {
+export const SplashScreen = ({ children, settings }) => {
   useEffect(() => {
     document.body.classList.remove('with-splash');
-    setTimeout(() => {
+    const removeSplash = () => {
       const splashScreen = document.querySelector('.splash-screen_container');
       splashScreen && splashScreen.remove();
+    };
+    if (settings?.theme?.splashScreenEnabled && settings?.theme?.splashScreen) {
+      const splashScreenImg = document.createElement('img');
+      splashScreenImg.src = settings?.theme?.splashScreen;
+      const splashScreenElement = document.querySelector('.splash-screen_container > .splash-screen');
+      splashScreenElement && splashScreenElement.appendChild(splashScreenImg);
+    }
+    setTimeout(() => {
+      removeSplash();
     }, 5000);
-  }, []);
+  }, [settings]);
   return children;
 };
 
-export default SplashScreen;
+export default connectSettings('settings')(SplashScreen);
