1e14372e1bd02d66711e0676b2897b63d1895c98
Maxime Bouveron <maxime.bouveron@gmail.com>
2025-07-10 14:09:16 +0200

    Change splashscreen timing + fix double splashcreen bug

--- src/views/Main/Content/SplashScreen.js
+++ src/views/Main/Content/SplashScreen.js
@@ -3,19 +3,26 @@ import { connectSettings } from '../Provider/context';
 
 export const SplashScreen = ({ children, settings }) => {
   useEffect(() => {
-    document.body.classList.remove('with-splash');
-    const removeSplash = () => {
+    const hideSplash = () => {
+      document.body.classList.remove('with-splash');
+    };
+    const unmountSplash = () => {
       const splashScreen = document.querySelector('.splash-screen_container');
       splashScreen && splashScreen.remove();
     };
     if (settings?.theme?.splashScreenEnabled && settings?.theme?.splashScreen) {
+      const existingImg = document.querySelector('.splash-screen_container img');
+      existingImg && existingImg.remove();
       const splashScreenImg = document.createElement('img');
       splashScreenImg.src = settings?.theme?.splashScreen;
       const splashScreenElement = document.querySelector('.splash-screen_container > .splash-screen');
       splashScreenElement && splashScreenElement.appendChild(splashScreenImg);
     }
     setTimeout(() => {
-      removeSplash();
+      hideSplash();
+    }, 2000);
+    setTimeout(() => {
+      unmountSplash();
     }, 5000);
   }, [settings]);
   return children;
