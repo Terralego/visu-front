6c6136b64d04b092ebcabaa7bfb934a8f8a76915
Maxime Bouveron <maxime.bouveron@gmail.com>
2026-05-04 13:16:43 +0200

    Whitespace issues

--- src/views/Visualizer/View/View.js
+++ src/views/Visualizer/View/View.js
@@ -272,7 +272,7 @@ export class Visualizer extends React.Component {
         const styleLegends =
           (styleLayer && styleLayer.advanced_style && styleLayer.advanced_style.legends) || [];
 
-        return [...layer.legends, ...(styleLegends || [])];
+        return [...(layer.legends || []), ...(styleLegends || [])];
       })
       .filter(defined => defined)
       .reduce(
@@ -1004,7 +1004,7 @@ export class Visualizer extends React.Component {
         theme: { logo, brandLogo } = {},
       },
       enableDetailCarrousel,
-onLoadLayerData,
+      onLoadLayerData,
     } = this.props;
 
     const {
@@ -1110,7 +1110,7 @@ onLoadLayerData,
           translate={t}
           layersExtent={bounds}
           isDetailsVisible={isDetailsVisible}
-onLoadLayerData={onLoadLayerData}
+          onLoadLayerData={onLoadLayerData}
         >
           <PrivateLayers layersTree={layersTree} />
           <div
